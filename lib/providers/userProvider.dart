94b37600600dd5ad83e0db0c6edbb469ae55f869
MossesAryo <mossesaryobimo@gmail.com>
2025-02-27 20:44:40 +0700

    `Added Firebase Firestore and Auth imports to UserProvider, implemented getUserDetails method, and updated ProfileScreen to use Provider for UserProvider.`

--- lib/providers/userProvider.dart
+++ lib/providers/userProvider.dart
@@ -1,8 +1,21 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class UserProvider extends ChangeNotifier {
   String userName = "";
   String userEmail = "";
   String userId = "";
-  
+
+  var db = FirebaseFirestore.instance;
+  var authUser = FirebaseAuth.instance.currentUser;
+
+  void getUserDetails() {
+    db.collection("users").doc(authUser!.uid).get().then((dataSnapshot) {
+      userName = dataSnapshot.data()?["name"] ?? "";
+      userId = dataSnapshot.data()?["id"] ?? "";
+      userEmail = dataSnapshot.data()?["email"] ?? "";
+      notifyListeners();
+    });
+  }
 }
