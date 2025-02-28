d68a04d5e1984216a27b04d578d7a723def167e6
MossesAryo <mossesaryobimo@gmail.com>
2025-02-28 11:01:26 +0700

    `Added Firebase Cloud Firestore integration and chatrooms list display to DashboardScreen`

--- lib/screens/dashboard_screen.dart
+++ lib/screens/dashboard_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:globalchat/screens/profile_screen.dart';
 import 'package:globalchat/screens/splash_screen.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class DashboardScreen extends StatefulWidget {
   const DashboardScreen({super.key});
@@ -12,58 +13,82 @@ class DashboardScreen extends StatefulWidget {
 
 class _DashboardScreenState extends State<DashboardScreen> {
   var user = FirebaseAuth.instance.currentUser;
+  var db = FirebaseFirestore.instance;
+
+  List<Map<String, dynamic>> chatroomsList = [];
+
+  void getChatrooms() {
+    db.collection("chatrooms").get().then((dataSnapshot) {
+      for (var singleChatroomData in dataSnapshot.docs) {
+        chatroomsList.add(singleChatroomData.data());
+
+        setState(() {});
+      }
+    });
+  }
+
+  @override
+  void initState() {
+    getChatrooms();
+    // TODO: implement initState
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Dashboard'),
-      ),
-      drawer: Drawer(
-        child: Container(
-          child: Column(
-            children: [
-              SizedBox(
-                height: 50,
-              ),
-              ListTile(
-                leading: Icon(Icons.people),
-                title: Text("Profile"),
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) {
-                      return ProfileScreen();
-                    }),
-                  );
-                },
-              ),
-              ListTile(
-                leading: Icon(Icons.logout),
-                title: Text("Logout"),
-                onTap: () async {
-                  await FirebaseAuth.instance.signOut();
-
-                  Navigator.pushAndRemoveUntil(context,
+        appBar: AppBar(
+          title: const Text('Dashboard'),
+        ),
+        drawer: Drawer(
+          child: Container(
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 50,
+                ),
+                ListTile(
+                  leading: Icon(Icons.people),
+                  title: Text("Profile"),
+                  onTap: () {
+                    Navigator.push(
+                      context,
                       MaterialPageRoute(builder: (context) {
-                    return SplashScreen();
-                  }), (route) {
-                    return false;
-                  });
-                },
-              )
-            ],
+                        return ProfileScreen();
+                      }),
+                    );
+                  },
+                ),
+                ListTile(
+                  leading: Icon(Icons.logout),
+                  title: Text("Logout"),
+                  onTap: () async {
+                    await FirebaseAuth.instance.signOut();
+
+                    Navigator.pushAndRemoveUntil(context,
+                        MaterialPageRoute(builder: (context) {
+                      return SplashScreen();
+                    }), (route) {
+                      return false;
+                    });
+                  },
+                )
+              ],
+            ),
           ),
         ),
-      ),
-      body: Center(
-        child: Column(
-          children: [
-            Text("Welcome,"),
-            Text((user?.email ?? "").toString()),
-          ],
-        ),
-      ),
-    );
+        body: ListView.builder(
+            itemCount: chatroomsList.length,
+            itemBuilder: (BuildContext context, int index) {
+              String chatroomName = chatroomsList[index]["chatroom_name"] ?? "";
+
+              return ListTile(
+                leading: CircleAvatar(
+                  child: Text(chatroomName[0]),
+                ),
+                title: Text(chatroomName),
+                subtitle: Text(chatroomsList[index]["chatroom_name"] ?? ""),
+              );
+            }));
   }
 }
